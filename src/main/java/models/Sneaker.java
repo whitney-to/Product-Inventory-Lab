717d7e48e2a34ebdefae672362bb25a3f2e8160b
Nguyet <nguyet@Zipcoders-MacBook-Pro-103.local>
2023-04-03 11:33:08 -0400

    added private fields for Sneaker class

--- src/main/java/models/Sneaker.java
+++ src/main/java/models/Sneaker.java
@@ -1,4 +1,11 @@
 package models;
 
 public class Sneaker {
+    private int id;
+    private String name;
+    private String brand;
+    private String sport;
+    private int size;
+    private int qty;
+    private float price;
 }
