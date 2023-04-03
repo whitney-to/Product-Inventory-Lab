ea02c96385f0f67cd9f33bc79396901d2e62a3a9
Nguyet <nguyet@Zipcoders-MacBook-Pro-103.local>
2023-04-03 15:56:35 -0400

    added tests for whiskey class

--- src/main/java/models/Whiskey.java
+++ src/main/java/models/Whiskey.java
@@ -1,4 +1,40 @@
 package models;
 
 public class Whiskey {
+    private int id;
+    private String brand;
+    private float price;
+
+    public Whiskey(){
+
+    }
+    public Whiskey(int id, String brand, float price) {
+        this.id = id;
+        this.brand = brand;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
 }
--- src/test/java/models/WhiskyTest.java
+++ src/test/java/models/WhiskyTest.java
@@ -1,4 +1,62 @@
 package models;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class WhiskyTest {
+    @Test
+    public void setBrandTest() {
+        // given (1)
+        String expected = "SomeBrand";
+
+        // when (2)
+        Whiskey testWhiskey = new Whiskey();
+        testWhiskey.setBrand(expected);
+
+        // then (3)
+        Assertions.assertEquals(expected, testWhiskey.getBrand());
+    }
+
+    @Test
+    public void setIdTest() {
+        // given (1)
+        Integer expected = 5;
+
+        // when (2)
+        Whiskey testWhiskey = new Whiskey();
+        testWhiskey.setId(expected);
+
+        // then (3)
+        Assertions.assertEquals(expected, testWhiskey.getId());
+    }
+
+    @Test
+    public void setPriceTest() {
+        // given (1)
+        Float expected = 5f;
+
+        // when (2)
+        Whiskey testWhiskey = new Whiskey();
+        testWhiskey.setPrice(expected);
+
+        // then (3)
+        Assertions.assertEquals(expected, testWhiskey.getPrice());
+    }
+
+    @Test // (1)
+    public void constructorTest(){
+
+        // (2)
+        int expectedId = 6;
+        String expectedBrand = "Adidas";
+        float expectedPrice = 80.00f;
+
+        // (3)
+        Whiskey testWhiskey = new Whiskey(expectedId, expectedBrand,expectedPrice);
+
+        // (4)
+        Assertions.assertEquals(expectedId, testWhiskey.getId());
+        Assertions.assertEquals(expectedBrand, testWhiskey.getBrand());
+        Assertions.assertEquals(expectedPrice, testWhiskey.getPrice());
+    }
 }
